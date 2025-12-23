04f781cde2793f8a84f7c10490a733d140a1414d
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2025-12-23 11:05:03 +0000

    CME-850 sampling at 1%

--- src/main/modules/appinsights/index.ts
+++ src/main/modules/appinsights/index.ts
@@ -7,10 +7,13 @@ export class AppInsights {
   enable(): void {
     if (config.get('appInsights.instrumentationKey')) {
       appInsights.setup(config.get('appInsights.instrumentationKey'))
-        .setSendLiveMetrics(true)
-        .start();
+        .setAutoDependencyCorrelation(true)
+        .setAutoCollectConsole(true, true)
+        .setSendLiveMetrics(true);
 
       appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'rpe-expressjs-template';
+      appInsights.defaultClient.config.samplingPercentage = 1;
+      appInsights.start();
       appInsights.defaultClient.trackTrace({message: 'App insights activated'});
     }
   }
