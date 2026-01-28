625e9fb1709899f4fa720ed68ead1a921909ffed
Chris Worthington <Christopher.Worthington@HMCTS.NET>
2026-01-28 14:20:32 +0000

    CME-850 only sample health and make consistent with other projects

--- src/main/app-insights/app-insights.ts
+++ src/main/app-insights/app-insights.ts
@@ -1,19 +1,36 @@
-const config = require('config');
-const appInsights = require('applicationinsights');
+const config = require("config");
+const appInsights = require("applicationinsights");
+
+const enabled = config.get("appInsights.enabled");
+
+function fineGrainedSampling(envelope) {
+  if (
+    ["RequestData", "RemoteDependencyData"].includes(envelope.data.baseType) &&
+    envelope.data.baseData.name.includes("/health")
+  ) {
+    envelope.sampleRate = 1;
+  }
+
+  return true;
+}
 
 const enableAppInsights = () => {
-  if (!config.get('appInsights.instrumentationKey')) {
-    return;
+  if (enabled) {
+    const appInsightsKey = config.get("secrets.ccd.AppInsightsInstrumentationKey");
+    const appInsightsRoleName = config.get("appInsights.roleName");
+    appInsights.setup(appInsightsKey)
+      .setAutoDependencyCorrelation(true)
+      .setAutoCollectRequests(true)
+      .setAutoCollectPerformance(true)
+      .setAutoCollectExceptions(true)
+      .setAutoCollectDependencies(true)
+      .setAutoCollectConsole(true)
+      .setUseDiskRetryCaching(true)
+      .setSendLiveMetrics(true);
+    appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = appInsightsRoleName;
+    appInsights.defaultClient.addTelemetryProcessor(fineGrainedSampling);
+    appInsights.start();
   }
-  const appInsightsKey = config.get('appInsights.instrumentationKey');
-  const appInsightsRoleName = 'ccpay-paymentoutcome-web';
-  appInsights.setup(appInsightsKey)
-    .setAutoDependencyCorrelation(true)
-    .setAutoCollectConsole(true, true);
-  appInsights.defaultClient.context.tags[
-    appInsights.defaultClient.context.keys.cloudRole] = appInsightsRoleName;
-  appInsights.defaultClient.config.samplingPercentage = 1;
-  appInsights.start();
 };
 
 module.exports = enableAppInsights;
