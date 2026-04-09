5a29eb9955eea21b565b9f270522f14b775e5308
Dave Jones <dave.jones@hmcts.net>
2026-04-09 20:15:57 +0100

    Increasing coverage after revert.

--- src/test/routes/home.ts
+++ src/test/routes/home.ts
@@ -1,21 +1,91 @@
 import { expect } from 'chai';
+import mock from 'nock';
 import request from 'supertest';
 
 import { app } from '../../main/app';
+import {
+  rejectGetPaymentStatus,
+  resolveCreateToken,
+  resolveGetPaymentStatusWithStatus
+} from '../http-mocks/fees';
 
-// TODO: replace this sample test with proper route tests for your application
 describe('Home page', () => {
-  describe('on GET', () => {
-    test('should return sample home page', async () => {
+  beforeEach(() => {
+    mock.cleanAll();
+    resolveCreateToken();
+  });
+
+  afterEach(() => {
+    mock.cleanAll();
+  });
+
+  describe('on GET /payment/:id/confirmation', () => {
+    test('renders English success page when payment status is Success', async () => {
+      resolveGetPaymentStatusWithStatus('Success');
+
       await request(app)
-        .get('/payment/:id/confirmation')
-        .expect((res) => expect(res.status).to.equal(200));
+        .get('/payment/466d7ea8-793b-4417-b4d7-a35b6b1a2fd6/confirmation?language=en')
+        .expect((res) => {
+          expect(res.status).to.equal(200);
+          expect(res.text).to.contain('Payment successful');
+          expect(res.text).to.contain('RC-1234-1234-1343-1234');
+          expect(res.text).to.contain('https://manage-case.platform.hmcts.net/cases/case-details/1234123412341234#Service Request');
+        });
+    });
+
+    test('renders Welsh success page when language is cy', async () => {
+      resolveGetPaymentStatusWithStatus('Success');
+
+      await request(app)
+        .get('/payment/466d7ea8-793b-4417-b4d7-a35b6b1a2fd6/confirmation?language=cy')
+        .expect((res) => {
+          expect(res.status).to.equal(200);
+          expect(res.text).to.contain('Taliad yn llwyddiannus');
+          expect(res.text).to.contain('Dychwelyd i gais gwasanaeth');
+        });
+    });
+
+    test('renders error summary when payment status is not Success', async () => {
+      resolveGetPaymentStatusWithStatus('Failed');
+
+      await request(app)
+        .get('/payment/466d7ea8-793b-4417-b4d7-a35b6b1a2fd6/confirmation?language=en')
+        .expect((res) => {
+          expect(res.status).to.equal(200);
+          expect(res.text).to.contain('There is a problem');
+          expect(res.text).to.contain('Your card payment was unsuccessful.');
+        });
+    });
+
+    test('renders English error summary when upstream payment API request fails', async () => {
+      rejectGetPaymentStatus();
+
+      await request(app)
+        .get('/payment/466d7ea8-793b-4417-b4d7-a35b6b1a2fd6/confirmation?language=en')
+        .expect((res) => {
+          expect(res.status).to.equal(200);
+          expect(res.text).to.contain('There is a problem');
+          expect(res.text).to.contain('Your card payment was unsuccessful.');
+        });
+    });
+
+    test('renders Welsh error summary when upstream payment API request fails', async () => {
+      rejectGetPaymentStatus();
+
+      await request(app)
+        .get('/payment/466d7ea8-793b-4417-b4d7-a35b6b1a2fd6/confirmation?language=cy')
+        .expect((res) => {
+          expect(res.status).to.equal(200);
+          expect(res.text).to.contain('Mae yna broblem');
+          expect(res.text).to.contain('Roedd eich taliad cerdyn yn aflwyddiannus.');
+        });
     });
   });
-  describe('on GET', () => {
-    test('should return sample error page', async () => {
+
+  describe('on GET unknown route', () => {
+    test('returns not found page', async () => {
       await request(app)
-        .get('/payment/:id/confirmat')
+        .get('/payment/466d7ea8-793b-4417-b4d7-a35b6b1a2fd6/confirmat')
         .expect((res) => expect(res.status).to.equal(404));
     });
   });
